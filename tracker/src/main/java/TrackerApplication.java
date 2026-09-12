487583a9bdd5d5e5095023bde97d6b04a7e384b7
devranaydinoglu <d.s.aydinoglu@gmail.com>
2026-09-12 12:54:26 +0200

    Removed unused imports

--- tracker/src/main/java/TrackerApplication.java
+++ tracker/src/main/java/TrackerApplication.java
@@ -1,13 +1,10 @@
-import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
 
 public class TrackerApplication {
 
     public static void main(String[] args) throws IOException {
         Tracker tracker = new Tracker(8080);
-
         tracker.start();
     }
 
