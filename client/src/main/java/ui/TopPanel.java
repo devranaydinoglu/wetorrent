93b937e3c06b8ebb4d1e4dce0198e523bfcff59f
devranaydinoglu <d.s.aydinoglu@gmail.com>
2026-09-18 17:33:55 +0200

    Added torrent selection and announce request

--- client/src/main/java/ui/TopPanel.java
+++ client/src/main/java/ui/TopPanel.java
@@ -1,18 +1,28 @@
 package ui;
 
+import bencode.Bencode;
+import network.AnnounceRequest;
+import network.TrackerClient;
+import org.apache.commons.codec.digest.DigestUtils;
+import torrent.Torrent;
+import torrent.TorrentCreator;
+import torrent.TorrentReader;
 import ui.torrent.NewTorrentDialog;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.IOException;
 
 public class TopPanel {
 
+    private final Window window;
     private final JPanel contentPane;
     private File torrentFile;
 
-    public TopPanel() {
+    public TopPanel(Window window) {
+        this.window = window;
         contentPane = new JPanel();
 
         FlowLayout flowLayout = new FlowLayout();
@@ -24,7 +34,7 @@ public class TopPanel {
         contentPane.add(newTorrentBtn);
 
         JButton downloadTorrentBtn = new JButton("Download");
-        downloadTorrentBtn.addActionListener(this::downloadTorrent);
+        downloadTorrentBtn.addActionListener(this::selectTorrent);
         contentPane.add(downloadTorrentBtn);
     }
 
@@ -37,14 +47,77 @@ public class TopPanel {
         newTorrentDialog.showDialog();
     }
 
-    public void downloadTorrent(ActionEvent e) {
+    public void selectTorrent(ActionEvent e) {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
         int returnVal = fileChooser.showOpenDialog(contentPane);
 
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             torrentFile = fileChooser.getSelectedFile();
-            System.out.println("File selected: " + torrentFile.getName());
+            String fileName = torrentFile.getName();
+            int lastIndexOf = fileName.lastIndexOf(".");
+
+            if (lastIndexOf == -1) {
+                JOptionPane.showMessageDialog(
+                    fileChooser,
+                    "Couldn't process file.",
+                    "File Processing Failed",
+                    JOptionPane.WARNING_MESSAGE
+                );
+                return;
+            }
+
+            String fileExtension = fileName.substring(lastIndexOf + 1);
+            if (!fileExtension.equals("torrent")) {
+                JOptionPane.showMessageDialog(
+                    fileChooser,
+                    "Selected file isn't a .torrent.",
+                    "File Not a Torrent",
+                    JOptionPane.WARNING_MESSAGE
+                );
+                return;
+            }
+
+            TorrentReader reader = new TorrentReader();
+            byte[] torrentFileBytes = reader.read(torrentFile);
+
+            TorrentCreator creator = new TorrentCreator();
+            Torrent torrent = creator.createFromBytes(torrentFileBytes);
+
+            try {
+                byte[] bencodedInfoDict = Bencode.encode(torrent.getTorrentInfo().toMap());
+
+                AnnounceRequest announceReq = new AnnounceRequest(
+                    DigestUtils.sha1(bencodedInfoDict),
+                    window.getPeerId(),
+                    6881,
+                    0,
+                    0,
+                    torrent.getTorrentInfo().getLength(),
+                    null,
+                    null
+                );
+
+                TrackerClient client = new TrackerClient();
+
+                try {
+                    client.sendAnnounceRequest(torrent.getAnnounce(), announceReq);
+                } catch (IOException | InterruptedException ex) {
+                    JOptionPane.showMessageDialog(
+                        fileChooser,
+                        "Couldn't connect with server.",
+                        "Server Connection Failed",
+                        JOptionPane.WARNING_MESSAGE
+                    );
+                }
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(
+                    fileChooser,
+                    "Couldn't process file.",
+                    "File Processing Failed",
+                    JOptionPane.WARNING_MESSAGE
+                );
+            }
         }
     }
 }
