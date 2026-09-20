0c43403865bf1df694ddd8b3033c50f72e72583a
devranaydinoglu <d.s.aydinoglu@gmail.com>
2026-09-20 16:06:04 +0200

    Added tracker response parsing

--- client/src/main/java/network/TrackerClient.java
+++ client/src/main/java/network/TrackerClient.java
@@ -9,6 +9,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 
 public class TrackerClient {
@@ -21,7 +22,7 @@ public class TrackerClient {
             .build();
     }
 
-    public void sendAnnounceRequest(String announceUrl, AnnounceRequest announceReq) throws IOException, InterruptedException {
+    public AnnounceResponse sendAnnounceRequest(String announceUrl, AnnounceRequest announceReq) throws IOException, InterruptedException {
         HttpRequest req = HttpRequest.newBuilder()
             .uri(announceReq.constructUri(announceUrl))
             .GET()
@@ -30,15 +31,24 @@ public class TrackerClient {
         HttpResponse<byte[]> res = client.send(req, HttpResponse.BodyHandlers.ofByteArray());
 
         try {
-            handleResponse(res.body());
+            return parseResponse(res.body());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void handleResponse(byte[] res) throws IOException {
+    private AnnounceResponse parseResponse(byte[] res) throws IOException {
         Map<String, Object> resMap = (Map<String, Object>)Bencode.decode(new ByteArrayInputStream(res));
-        System.out.println(resMap);
+
+        if (resMap.containsKey("failure reason")) {
+            return new AnnounceResponse((String)resMap.get("failure reason"));
+        } else if (resMap.containsKey("interval") && resMap.containsKey("peers")) {
+            Integer interval = Integer.valueOf(((Long)resMap.get("interval")).intValue());
+            List<Peer> peers = (List<Peer>)resMap.get("peers");
+            return new AnnounceResponse(interval, peers);
+        } else {
+            throw new IllegalArgumentException("Missing required query parameter");
+        }
     }
 
 }
