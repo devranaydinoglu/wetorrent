7dcbd3572aede4794173a6cc1c5c4bccaa574f59
devranaydinoglu <d.s.aydinoglu@gmail.com>
2026-09-23 17:30:12 +0200

    Integrated new Session, TrackerClient, and use of ExecutorService

--- client/src/main/java/ClientApplication.java
+++ client/src/main/java/ClientApplication.java
@@ -1,9 +1,20 @@
+
+import base.Session;
+import network.TrackerClient;
 import ui.Window;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ClientApplication {
 
     public static void main(String[] args) {
-        Window window = new Window();
+        ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+        TrackerClient trackerClient = new TrackerClient();
+
+        Session session = new Session(trackerClient, executorService);
+
+        Window window = new Window(session);
         window.start();
     }
 
