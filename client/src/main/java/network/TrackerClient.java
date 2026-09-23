090d1a8d324e50c4ab83262f33c60190e192b58b
devranaydinoglu <d.s.aydinoglu@gmail.com>
2026-09-23 17:32:45 +0200

    Fixed parsing bug where the peers list wasn't converted to Peer objects

--- client/src/main/java/network/TrackerClient.java
+++ client/src/main/java/network/TrackerClient.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -41,10 +43,23 @@ public class TrackerClient {
         Map<String, Object> resMap = (Map<String, Object>)Bencode.decode(new ByteArrayInputStream(res));
 
         if (resMap.containsKey("failure reason")) {
-            return new AnnounceResponse((String)resMap.get("failure reason"));
+            String reason = new String((byte[]) resMap.get("failure reason"), StandardCharsets.UTF_8);
+            return new AnnounceResponse(reason);
         } else if (resMap.containsKey("interval") && resMap.containsKey("peers")) {
             Integer interval = Integer.valueOf(((Long)resMap.get("interval")).intValue());
-            List<Peer> peers = (List<Peer>)resMap.get("peers");
+
+            Object rawPeers = resMap.get("peers");
+            List<Peer> peers = new ArrayList<>();
+            for (Object peerObj : (List<?>)rawPeers) {
+                Map<String, Object> peerMap = (Map<String, Object>)peerObj;
+
+                byte[] peerId = (byte[])peerMap.get("peer id");
+                String ip = new String((byte[]) peerMap.get("ip"), StandardCharsets.UTF_8);;
+                int port = ((Long)peerMap.get("port")).intValue();
+
+                peers.add(new Peer(peerId, ip, port));
+            }
+
             return new AnnounceResponse(interval, peers);
         } else {
             throw new IllegalArgumentException("Missing required query parameter");
